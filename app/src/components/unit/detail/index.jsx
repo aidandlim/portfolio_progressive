2ea576dcf4351efcd652e473d51a4c3c7470240b
aidandlim <aidandlim@gmail.com>
2019-12-19 14:51:59 +0900

    update modules

--- app/src/components/unit/detail/index.jsx
+++ app/src/components/unit/detail/index.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import queryString from 'query-string';
 
 import './index.css';
 
-const Detail = () => {
+const Detail = ({ location }) => {
+	const [ projectDetail, setProjectDetail ] = useState({});
+
+	useEffect(() => {
+		const { id } = queryString.parse(location.search);
+		console.log(id);
+	}, [location.search]);
+
 	return (
 		<div className='detail'>
 			detail
