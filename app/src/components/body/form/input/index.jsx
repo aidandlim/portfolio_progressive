4d87f445ab14550c300b681541981bdf783b55e9
aidandlim <aidandlim@gmail.com>
2019-12-23 17:44:47 +0900

    update app & server

--- app/src/components/body/form/input/index.jsx
+++ app/src/components/body/form/input/index.jsx
@@ -11,21 +11,26 @@ const Input = ({ element }) => {
     } else if(element.type === 'people') {
         className = 'body-form-label-medium';
     }
+
     return (
+        element.type === 'people' 
+        ? 
+        <div className={className}>
+            <p className='body-form-title'>{element.title} {element.isRequired ? '*' : null}</p>
+            <div className='body-form-list'>
+                { element.users.length === 0 ? '회원정보 로딩 중입니다.' : element.users.map((user, index) => 
+                    <div className='body-form-list-element' key={index}>
+                        <input className='body-form-checkbox' type='checkbox' name={element.name} value={user.id} />
+                        <div className='body-form-checkbox-title'>{user.name}</div>
+                    </div>    
+                )}
+            </div>
+        </div>
+        : 
         <label className={className}>
             <p className='body-form-title'>{element.title} {element.isRequired ? '*' : null}</p>
             { inputList.indexOf(element.type) > -1 ? <input className='body-form-input' type={element.type} name={element.name} required={element.isRequired ? 'required' : ''} /> : null }
             { element.type === 'textarea' ? <textarea className='body-form-textarea' name={element.name}></textarea> : null }
-            { element.type === 'people' ? 
-                <div className='body-form-list'>
-                    { element.users.length === 0 ? '회원정보 로딩 중입니다.' : element.users.map((user, index) => 
-                        <div className='body-form-list-element' key={index}>
-                            <input className='body-form-checkbox' type='checkbox' name={element.name} value={user.id} />
-                            <div className='body-form-checkbox-title'>{user.name}</div>
-                        </div>    
-                    )}
-                </div>
-            : null }
         </label>
     );
 }
