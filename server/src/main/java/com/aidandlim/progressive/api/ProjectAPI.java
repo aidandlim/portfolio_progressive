a4819c7b374a511ac9f78115e697d4392ed0fa2d
aidandlim <aidandlim@gmail.com>
2019-12-26 14:06:35 +0900

    update app & server

--- server/src/main/java/com/aidandlim/progressive/api/ProjectAPI.java
+++ server/src/main/java/com/aidandlim/progressive/api/ProjectAPI.java
@@ -1,14 +1,6 @@
 package com.aidandlim.progressive.api;
 
-import com.aidandlim.progressive.dto.User;
-import com.aidandlim.progressive.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 
 @RestController
 public class ProjectAPI {
